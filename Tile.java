b32ce1732fb7e8bd8adca2d4144c81b7737e13c4
Raymond Zhang <dracorz401@gmail.com>
2024-06-10 23:15:13 -0400

    Change dimensions to remove empty rows/cols

--- Tile.java
+++ Tile.java
@@ -10,116 +10,113 @@ public class Tile {
     // private int x = -1;
     // private int y = -1;
     private final int points;
-    private final int size;
+    private int rows;
+    private int cols;
     private boolean used;
     private boolean[][] squares;
 
     /*
      * Method name: Tile()
      * Parameters: int points - The point value of the tile
-     *             int size - The length of the square grid containing the tile
-     *             boolean[][] squares - The square grid representing the squares occupied by the tile
-     * Description: Constructor method that defines the point value, size, and squares occupied by the tile.
+     * int size - The length of the square grid containing the tile
+     * boolean[][] squares - The square grid representing the squares occupied by
+     * the tile
+     * Description: Constructor method that defines the point value, size, and
+     * squares occupied by the tile.
      */
-    public Tile(int points, int size, boolean[][] squares) {
+    public Tile(int points, int rows, int cols, boolean[][] squares) {
         this.points = points;
-        this.size = size;
+        this.rows = rows;
+        this.cols = cols;
         this.squares = squares;
     }
 
-    // Accessor methods
     /*
-     * 
+     * Method name: getPoints()
+     * Return type: int - The point-value of the Tile instance
+     * Description: Accessor method for the points attribute in the Tile class.
+     */
+    public int getPoints() {
+        return points;
+    }
+
+    /*
+     * Method name: getRows()
+     * Return type: int - The length of the Tile instance
+     * Description: Accessor method for the rows attribute in the Tile class.
+     */
+    public int getRows() {
+        return rows;
+    }
+
+    /*
+     * Method name: getCols()
+     * Return type: int - The width of the Tile instance
+     * Description: Accessor method for the cols attribute in the Tile class.
+     */
+    public int getCols() {
+        return cols;
+    }
+
+    /*
+     * Method name: getSquares()
+     * Return type: boolean[][] - The square area taken up by the Tile instance
+     * Description: Accessor method for the squares attribute in the Tile class.
+     */
+    public boolean[][] getSquares() {
+        return squares;
+    }
+
+    // public boolean[][] getSquaresCopy() {
+    //     boolean[][] squaresCopy = new boolean[getRows()][getCols()];
+
+    //     // Copy the squares array
+    //     for (int i = 0; i < getRows(); i++) {
+    //         System.arraycopy(getSquares()[i], 0, squaresCopy[i], 0, getCols());
+    //     }
+    //     return squaresCopy;
+    // }
+
+    /*
+     * Method name: getUsed()
+     * Return type: boolean - Whether or not the Tile instance has been used in game.
+     * Description: Accessor method for the used attribute in the Tile class.
      */
     public boolean getUsed() {
         return used;
     }
 
-    public int getPoints() {
-        return points;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public boolean[][] getSquares() {
-        return squares;
-    }
-
-    public boolean[][] getSquaresCopy() {
-        boolean[][] squaresCopy = new boolean[getSize()][getSize()];
-
-        // Copy the squares array
-        for (int i = 0; i < getSize(); i++) {
-            System.arraycopy(getSquares()[i], 0, squaresCopy[i], 0, getSize());
-        }
-        return squaresCopy;
+    /*
+     * Method name: setRows()
+     * Parameters: int rows - The new number of rows
+     * Description: Mutator method for the rows attribute in the Tile class.
+     */
+    public void setRows(int rows) {
+        this.rows = rows;
     }
 
     /*
-     * Method name: getSquaresRemoveEmpty()
-     * Return type: boolean[][] - A new grid of squares representing the tile with empty rows/columns removed
-     * Description: Returns a copy of the squares array with empty rows and columns removed.
+     * Method name: setCols()
+     * Parameters: int cols - The new number of columns
+     * Description: Mutator method for the cols attribute in the Tile class.
      */
-    public boolean[][] getSquaresRemoveEmpty() {
-        // Declare variables
-        int rowCount = 0, colCount = 0, maxRow = 0, maxCol = 0, rowIdx = getSize(), colIdx = getSize();
-        boolean[][] newSquares;
-
-        // Find the empty rows or columns
-        for(int i = 0; i<getSize(); i++) {
-            for(int j = 0; j<getSize();j++) {
-                // Square found in row j
-                if(getSquares()[j][i]) {
-                    // Index of first row that is not empty
-                    rowIdx = Math.min(rowIdx, j);
-
-                    rowCount++;
-                }
-                // Square found in column j
-                if(getSquares()[i][j]) {
-                    // Index of first column that is not empty
-                    colIdx = Math.min(colIdx, j);
-
-                    colCount++;
-                }
-            }
-            // Get the new maximum amount of rows needed and reset counter
-            maxRow = Math.max(maxRow, rowCount);
-            rowCount = 0;
-
-            // Get the new maximum amount of columns needed and reset counter
-            maxCol = Math.max(maxCol, colCount);
-            colCount = 0;
-        }
-
-        // Initialize new array
-        newSquares = new boolean[maxRow][maxCol];
-
-        for(int i = rowIdx; i<rowIdx + maxRow; i++) {
-            for(int j = colIdx; j<colIdx + maxCol; j++) {
-                newSquares[i-rowIdx][j-colIdx] = getSquares()[i][j];
-            }
-        }
-
-        return newSquares;
+    public void setCols(int cols) {
+        this.cols = cols;
     }
 
     /*
      * Method name: setSquares()
-     * Parameters: boolean[][] newSquares
-     * Description: Mutator method for squares.
+     * Parameters: boolean[][] newSquares - The new tile area
+     * Description: Mutator method for the squares attribute in the Tile class.
      */
-    // Precondition: newSquares is the same size as squares
     public void setSquares(boolean[][] newSquares) {
         squares = newSquares;
     }
 
     /*
      * Method name: setUsed()
-     * Parameters: boolean used - The new boolean variable to assign.
-     * Description: Mutator method for the used condition.
+     * Parameters: boolean used - The new used state.
+     * Description: Mutator method for the used attribute in the Tile class.
      */
     public void setUsed(boolean used) {
         this.used = used;
@@ -130,12 +127,16 @@ public class Tile {
      * Description: Rotates the tile clockwise.
      */
     public void rotateRight() {
-        boolean[][] newSquares = new boolean[getSize()][getSize()];
-        for (int i = 0; i < getSize(); i++) {
-            for (int j = 0; j < getSize(); j++) {
-                newSquares[i][j] = getSquares()[getSize() - j - 1][i];
+        // Create new squares with reversed dimensions
+        boolean[][] newSquares = new boolean[getCols()][getRows()];
+
+        for (int i = 0; i < getCols(); i++) {
+            for (int j = 0; j < getRows(); j++) {
+                newSquares[i][j] = getSquares()[getRows() - j - 1][i];
             }
         }
+
+        // Update squares
         setSquares(newSquares);
     }
 
@@ -144,18 +145,21 @@ public class Tile {
      * Description: Rotates the tile counterclockwise.
      */
     public void rotateLeft() {
-        boolean[][] newSquares = new boolean[getSize()][getSize()];
-        for (int i = 0; i < getSize(); i++) {
-            for (int j = 0; j < getSize(); j++) {
-                newSquares[i][j] = getSquares()[j][getSize() - i - 1];
+        // Create new squares with reversed dimensions
+
+        boolean[][] newSquares = new boolean[getCols()][getRows()];
+        for (int i = 0; i < getCols(); i++) {
+            for (int j = 0; j < getRows(); j++) {
+                newSquares[i][j] = getSquares()[j][getRows() - i - 1];
             }
         }
+
+        // Update squares
         setSquares(newSquares);
     }
 
     /*
      * Method name: flipVert()
-     * Method type: Non-static
      * Description: Flips the tile vertically
      */
     public void flipVert() {
@@ -163,19 +167,18 @@ public class Tile {
         boolean[][] flippedSquares = getSquares();
 
         // Loop throw tile's columns
-        for (int i = 0; i < getSize(); i++) {
-            for (int j = 0; j < getSize() / 2; j++) {
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < getCols() / 2; j++) {
                 // Swap top rows with bottom rows
-                flippedSquares[j][i] ^= flippedSquares[getSize() - j - 1][i];
-                flippedSquares[getSize() - j - 1][i] ^= flippedSquares[j][i];
-                flippedSquares[j][i] ^= flippedSquares[getSize() - j - 1][i];
+                flippedSquares[j][i] ^= flippedSquares[getCols() - j - 1][i];
+                flippedSquares[getCols() - j - 1][i] ^= flippedSquares[j][i];
+                flippedSquares[j][i] ^= flippedSquares[getCols() - j - 1][i];
             }
         }
     }
 
     /*
      * Method name: flipHorz()
-     * Method type: Non-static
      * Description: Flips the tile horizontally
      */
     public void flipHorz() {
@@ -183,31 +186,30 @@ public class Tile {
         boolean[][] flippedSquares = getSquares();
 
         // Loop through tile's rows
-        for (int i = 0; i < getSize(); i++) {
-            for (int j = 0; j < getSize() / 2; j++) {
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < getCols() / 2; j++) {
                 // Swap left columns with right columns
-                flippedSquares[i][j] ^= flippedSquares[i][getSize() - j - 1];
-                flippedSquares[i][getSize() - j - 1] ^= flippedSquares[i][j];
-                flippedSquares[i][j] ^= flippedSquares[i][getSize() - j - 1];
+                flippedSquares[i][j] ^= flippedSquares[i][getCols() - j - 1];
+                flippedSquares[i][getCols() - j - 1] ^= flippedSquares[i][j];
+                flippedSquares[i][j] ^= flippedSquares[i][getCols() - j - 1];
             }
         }
     }
 
     /*
      * Method name: placeTile()
-     * Method type: non-static
      * Parameters: int r - The row of the tile location. Assumed to be valid.
-     *             int c - The column of the tile location. Assumed to be valid.
-     *             char currentPlayer - The character representing the current player
-     *             char[][] - The game board
+     * int c - The column of the tile location. Assumed to be valid.
+     * char currentPlayer - The character representing the current player
+     * char[][] - The game board
      * Description: Places a tile onto a valid location on the game board.
      */
     public void placeTile(int r, int c, char currentPlayer, char[][] board) {
         // Loop through piece at (r,c)
-        for (int i = r; i < r + getSize(); i++) {
-            for (int j = c; j < c + getSize(); j++) {
+        for (int i = r; i < r + getRows(); i++) {
+            for (int j = c; j < c + getCols(); j++) {
                 // Square in tile is not empty
-                if (getSquares()[i-r][j-c]) {
+                if (getSquares()[i - r][j - c]) {
                     board[i][j] = currentPlayer;
                 }
             }
@@ -216,7 +218,6 @@ public class Tile {
 
     /*
      * Method name: newTileSet()
-     * Method type: static
      * Return type: Map<String,Tile> - A set of 21 key-value pairs, each
      * representing a unique Blokus tile. The String represents its identifier name,
      * while Tile represents the data contained within the tile.
@@ -225,50 +226,47 @@ public class Tile {
     public static Map<String, Tile> newTileSet() {
         Map<String, Tile> newTiles = new HashMap<>();
 
-        newTiles.put("I1", new Tile(1, 1, new boolean[][] { { true } }));
-        newTiles.put("I2", new Tile(2, 2, new boolean[][] { { true, true }, { false, false } }));
-        newTiles.put("V3", new Tile(3, 2, new boolean[][] { { true, false }, { true, true } }));
-        newTiles.put("I3", new Tile(3, 3,
-                new boolean[][] { { false, false, false }, { true, true, true, }, { false, false, false } }));
-        newTiles.put("O", new Tile(4, 2, new boolean[][] { { true, true }, { true, true } }));
-        newTiles.put("T4", new Tile(4, 3,
-                new boolean[][] { { false, false, false }, { true, true, true }, { false, true, false } }));
-        newTiles.put("L4", new Tile(4, 3,
-                new boolean[][] { { true, false, false }, { true, true, true }, { false, false, false } }));
-        newTiles.put("Z4", new Tile(4, 3,
-                new boolean[][] { { false, false, false }, { true, true, false }, { false, true, true } }));
-        newTiles.put("I4", new Tile(4, 4,
-                new boolean[][] { { false, false, false, false }, { true, true, true, true },
-                        { false, false, false, false }, { false, false, false, false } }));
-        newTiles.put("F", new Tile(5, 3,
+        newTiles.put("I1", new Tile(1, 1, 1, new boolean[][] { { true } }));
+        newTiles.put("I2", new Tile(2, 1, 2, new boolean[][] { { true, true } }));
+        newTiles.put("V3", new Tile(3, 2, 2, new boolean[][] { { true, false }, { true, true } }));
+        newTiles.put("I3", new Tile(3, 1, 3,
+                new boolean[][] { { true, true, true, } }));
+        newTiles.put("O", new Tile(4, 2, 2, new boolean[][] { { true, true }, { true, true } }));
+        newTiles.put("T4", new Tile(4, 2, 3,
+                new boolean[][] { { true, true, true }, { false, true, false } }));
+        newTiles.put("L4", new Tile(4, 2, 3,
+                new boolean[][] { { true, false, false }, { true, true, true } }));
+        newTiles.put("Z4", new Tile(4, 2, 3,
+                new boolean[][] { { true, true, false }, { false, true, true } }));
+        newTiles.put("I4", new Tile(4, 1, 4,
+                new boolean[][] { { true, true, true, true } }));
+        newTiles.put("F", new Tile(5, 3, 3,
                 new boolean[][] { { false, true, false }, { true, true, true }, { true, false, false } }));
-        newTiles.put("X", new Tile(5, 3,
+        newTiles.put("X", new Tile(5, 3, 3,
                 new boolean[][] { { false, true, false }, { true, true, true }, { false, true, false } }));
-        newTiles.put("P", new Tile(5, 3,
-                new boolean[][] { { true, true, false }, { true, true, false }, { true, false, false } }));
-        newTiles.put("W", new Tile(5, 3,
+        newTiles.put("P", new Tile(5, 3, 2,
+                new boolean[][] { { true, true }, { true, true }, { true, false } }));
+        newTiles.put("W", new Tile(5, 3, 3,
                 new boolean[][] { { true, false, false }, { true, true, false }, { false, true, true } }));
-        newTiles.put("Z5", new Tile(5, 3,
+        newTiles.put("Z5", new Tile(5, 3, 3,
                 new boolean[][] { { true, true, false }, { false, true, false }, { false, true, true } }));
-        newTiles.put("U", new Tile(5, 3,
-                new boolean[][] { { true, false, true }, { true, true, true }, { false, false, false } }));
-        newTiles.put("T5", new Tile(5, 3,
+        newTiles.put("U", new Tile(5, 2, 3,
+                new boolean[][] { { true, false, true }, { true, true, true } }));
+        newTiles.put("T5", new Tile(5, 3, 3,
                 new boolean[][] { { true, true, true }, { false, true, false }, { false, true, false } }));
-        newTiles.put("V5", new Tile(5, 3,
+        newTiles.put("V5", new Tile(5, 3, 3,
                 new boolean[][] { { true, false, false }, { true, false, false }, { true, true, true } }));
-        newTiles.put("Y", new Tile(5, 4,
-                new boolean[][] { { false, false, false, false }, { false, true, false, false },
-                        { true, true, true, true }, { false, false, false, false } }));
-        newTiles.put("L5", new Tile(5, 4,
-                new boolean[][] { { false, false, false, false }, { true, false, false, false },
-                        { true, true, true, true }, { false, false, false, false } }));
-        newTiles.put("N", new Tile(5, 4,
-                new boolean[][] { { false, false, false, false }, { false, true, true, true },
-                        { true, true, false, false }, { false, false, false, false } }));
-        newTiles.put("I5", new Tile(5, 5,
-                new boolean[][] { { false, false, false, false, false }, { false, false, false, false, false },
-                        { true, true, true, true, true }, { false, false, false, false, false },
-                        { false, false, false, false, false } }));
+        newTiles.put("Y", new Tile(5, 2, 4,
+                new boolean[][] { { false, true, false, false },
+                        { true, true, true, true } }));
+        newTiles.put("L5", new Tile(5, 2, 4,
+                new boolean[][] { { true, false, false, false },
+                        { true, true, true, true } }));
+        newTiles.put("N", new Tile(5, 2, 4,
+                new boolean[][] { { false, true, true, true },
+                        { true, true, false, false } }));
+        newTiles.put("I5", new Tile(5, 1, 5,
+                new boolean[][] { { true, true, true, true, true } }));
 
         return newTiles;
     }
